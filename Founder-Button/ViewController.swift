b3432bf0a6ccf24e311a7a871223df5e1e201840
Cathy Chen <chencfb@bc.edu>
2019-08-29 18:47:13 -0400

    Founder button with google and rr

--- Founder-Button/ViewController.swift
+++ Founder-Button/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBOutlet weak var label2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func googleButton(_ sender: Any) {
+       label1.text = "Larry Page"
+        label2.text = "Sergey Brin"
+    }
+    
+    @IBAction func rrButton(_ sender: Any) {
+        label1.text = "Jennifer Hyman"
+        label2.text = "Jenny Fleiss"
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        label1.text = ""
+        label2.text = ""
+    }
 }
 
